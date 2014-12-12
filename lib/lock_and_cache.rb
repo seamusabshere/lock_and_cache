bb8206c649819872e0034e2bf2a4341d78ba94e4
Seamus Abshere <seamus@abshere.net>
2014-12-12 20:20:06 -0200

    ENV['LOCK_AND_CACHE_DEBUG'] == 'true' debug output to $stderr

--- lib/lock_and_cache.rb
+++ lib/lock_and_cache.rb
@@ -12,14 +12,20 @@ module LockAndCache
     alias_method unlocked_method_id, method_id
 
     define_method method_id do |*args|
+      debug = (ENV['LOCK_AND_CACHE_DEBUG'] == 'true')
       lock_key = [self.class.name, method_id, HashDigest.digest3([as_cache_key]+args)].join('/')
+      debug_lock_key = [self.class.name, method_id, [as_cache_key]+args].join('/') if debug
+      Thread.exclusive { $stderr.puts "[lock_and_cache] A #{debug_lock_key}" } if debug
       if cache_method_cached?(method_id, args)
         return send(method_id, *args) # which will be the cached version
       end
+      Thread.exclusive { $stderr.puts "[lock_and_cache] B #{debug_lock_key}" } if debug
       ActiveRecord::Base.with_advisory_lock(lock_key) do
+        Thread.exclusive { $stderr.puts "[lock_and_cache] C #{debug_lock_key}" } if debug
         if cache_method_cached?(method_id, args)
           send method_id, *args # which will be the cached version
         else
+          Thread.exclusive { $stderr.puts "[lock_and_cache] D #{debug_lock_key}" } if debug
           send unlocked_method_id, *args
         end
       end
--- lib/lock_and_cache/version.rb
+++ lib/lock_and_cache/version.rb
@@ -1,3 +1,3 @@
 module LockAndCache
-  VERSION = '0.0.4'
+  VERSION = '0.0.5'
 end
